8e84b7b5142d5fb9706b178b3eebe44a33feac8a
lucasdemouramacedo <lucasdemouramacedo@gmail>
2025-08-17 18:22:50 -0300

    first commit

---
+++ api/src/main/java/com/power/power_pdf/PowerPdfApplication.java
@@ -0,0 +1,13 @@
+package com.power.power_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerPdfApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/power/power_pdf/PowerPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.power.power_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
