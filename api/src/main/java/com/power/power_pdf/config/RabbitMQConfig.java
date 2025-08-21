aaeb3c89105c723334bfa2d340414efd3b16c87e
lucasdemouramacedo <lucasdemouramacedo@gmail>
2025-08-21 01:22:23 -0300

    feat: add RabbitMQ config

---
+++ api/src/main/java/com/power/power_pdf/config/RabbitMQConfig.java
@@ -0,0 +1,31 @@
+package com.power.power_pdf.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "power_pdf_queue";
+    public static final String EXCHANGE_NAME = "power_pdf_exchange";
+    public static final String ROUTING_KEY =  "power_pdf_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, true);
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
