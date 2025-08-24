3995f9b8ebecdf5f201008a3750cca61ebc274ad
lucasdemouramacedo <lucasdemouramacedo@gmail>
2025-08-24 19:58:41 -0300

    feat: add prop label to link

--- app/src/components/Link.tsx
+++ app/src/components/Link.tsx
@@ -1,20 +1,22 @@
 type Props = {
     link: string;
+    enableLabel: string;
+    disableLabel: string;
 }
 
-export default function Link({ link }: Props) {
+export default function Link( props: Props) {
 
     return (
         <div>
-            { link && (
-                <a href={link} className="inline-block text-center text-white bg-blue-500 py-[2px] w-[130px] rounded-lg font-normal text-sm no-underline">
-                    Download
+            { props.link && (
+                <a href={ props.link} className="inline-block text-center text-white bg-blue-500 py-[2px] w-[130px] rounded-lg font-normal text-sm no-underline">
+                    {props.enableLabel}
                 </a>
             )}
 
-            { !link && (
+            { ! props.link && (
                 <div className=" text-center bg-white text-blue-500 py-[0px] w-[130px] rounded-lg font-normal text-sm border-2 border-dotted border-blue-500">
-                    Pendente
+                    {props.disableLabel}
                 </div>
             )}
         </div>
